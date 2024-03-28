8f0ba7799a0d9b60e29f6234a588e412045ec563
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2024-03-28 22:30:29 +0330

    added navbar

---
+++ sharedScript.js
@@ -0,0 +1,6 @@
+function handleMenuButtonClick() {
+  document.querySelector(".nav-menu").classList.toggle("nav-menu-open")
+  document
+    .querySelector(".nav-menu-button")
+    .classList.toggle("nav-menu-button-close")
+}
