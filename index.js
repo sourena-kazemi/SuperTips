81f1ceaf17948276cabebc043fc778b42ac72618
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2024-04-09 22:33:21 +0330

    finish mobile design of homepage

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function handleDropdownClick(dropdownClass) {
   document.querySelector(`.${dropdownClass}`).classList.toggle("dropdown-open")
 }
+
+let reviews = document.querySelectorAll(".review")
+let reviewSelectorButtons = document.querySelectorAll(".selector-button")
+function handleReviewSelectorClick(reviewId) {
+  reviews.forEach((review, index) => {
+    if (index + 1 === reviewId) {
+      review.classList.remove("review-hide")
+      reviewSelectorButtons[index].classList.add("selector-button-active")
+    } else {
+      review.classList.add("review-hide")
+      reviewSelectorButtons[index].classList.remove("selector-button-active")
+    }
+  })
+}
