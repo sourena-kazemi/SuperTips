b0d3f9e6ee3eb7303bdb5d1c32d3c26187aa602b
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2024-04-27 22:35:53 +0330

    finish dashboard page menu

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,8 +1,28 @@
-function handleChangeFilterButtonClick() {
+function handleMenuButtonClick() {
   document
     .querySelector(".sidebar-menu")
     .classList.toggle("sidebar-menu-active")
+  document
+    .querySelector(".sidebar-filter")
+    .classList.toggle("sidebar-filter-hidden")
 }
 function handleDropdownClick(dropdownClass) {
   document.querySelector(`.${dropdownClass}`).classList.toggle("dropdown-open")
+}
+let currentItem = { sport: "soccer", id: 4 }
+function handleDropdownItemClick(itemSport, itemId) {
+  document
+    .querySelectorAll(`.${currentItem.sport}-league`)
+    [currentItem.id - 1].classList.remove("selected-item")
+  document
+    .querySelectorAll(`.${itemSport}-league`)
+    [itemId - 1].classList.add("selected-item")
+  currentItem.sport = itemSport
+  currentItem.id = itemId
+  document.querySelector(".selected-sport").innerHTML = currentItem.sport
+  document.querySelector(".selected-league").innerHTML =
+    document.querySelectorAll(`.${currentItem.sport}-league`)[
+      currentItem.id - 1
+    ].innerHTML
+  handleMenuButtonClick()
 }
